2127beef94b10273e03f1af6ba7cd13ec76db283
App <farmbot622@gmail.com>
2026-04-06 16:22:24 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,16 @@ const urlsToCache = [
     '/style.css',
     '/main.js',
     '/config.js',
-    '/favicon.ico',
+    '/manifest.json',
+    '/images/favicon.ico',
+    '/images/icon-72.png',
+    '/images/icon-96.png',
+    '/images/icon-128.png',
+    '/images/icon-144.png',
+    '/images/icon-152.png',
+    '/images/icon-192.png',
+    '/images/icon-384.png',
+    '/images/icon-512.png',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
